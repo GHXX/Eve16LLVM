cb8a38e1ed0e3101ba2a46b4dff683bc63abbc66
Artyom Y. Razinov <artyom.razinov@gmail.com>
2024-12-03 12:25:15 +0300

    Update RISCW backend
    This is a source code: https://github.com/andresag01/llvm-project
    
    This is an article: https://sourcecodeartisan.com/2020/09/13/llvm-backend-0.html
    
    Source code was exactly 4 years old (Dec 3, 2020 vs Dec 3, 2024) so it needed updates.
    
    I rebased commits from andresag01’s repo onto main, because andresag01’s repo was like 487 commits ahead of llvm/llvm-project:main, like it wasn’t forked from main branch
    
    So andresag01’s don’t work, however, they match his article, so I kept them more or less intact (I fixed conflicts)
    
    Feel free to rewrite history of this branch, I created this branch to share my work of updating andresag01’s code, while my goal was to make my own backend, I’ll remove this commit (not the code) in the code of my backend
    
    I used ninja to build project on Mac OS and used Xcode to debug llvm (I failed to open clang with Xcode).
    
    I had this in my temp directory:
    
    ```
    
    cd "$(dirname $0)"
    
    mode=$1
    
    target_name='RISCW'
    llvm_code_location=~/src/llvm-project/llvm
    
    rm -rf xcode
    mkdir xcode
    cd xcode
    cmake \
    	-G Xcode \
    	-DCMAKE_BUILD_TYPE=Debug \
    	"$llvm_code_location" \
    	-DLLVM_TARGETS_TO_BUILD="$target_name" \
    	-DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD="$target_name" \
    	-DCMAKE_BUILD_TYPE="Debug" \
    	-DLLVM_ENABLE_ASSERTIONS=On
    
    if [ "$mode" == "open" ]; then
    	open "LLVM.xcodeproj"
    fi
    ```
    
    To make xcode project. I failed to build clang using Xcode.
    
    This is how I built llvm and clang using ninja (with prerequisites; inside a temp directory `ninja`):
    
    ```
    brew install ninja
    
    cmake -G Ninja ~/src/llvm-project/llvm -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=RISCW -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=RISCW -DLLVM_ENABLE_PROJECTS="clang"
    
    ninja
    ```
    
    I also had this in other temporary directory (`demo`):
    
    fixture.c:
    
    ```
    int test(int a, int b)
    {
        return a + b;
    }
    ```
    
    Makefile:
    
    ```
    fixture.bc:
    	../ninja/bin/clang -O2 -emit-llvm -target riscw -c fixture.c -o fixture.bc
    
    fixture.ll: fixture.bc
    	../ninja/bin/llvm-dis fixture.bc -o fixture.ll
    
    fixture.S: fixture.bc
    	../ninja/bin/llc -march=riscw -O2 -filetype=asm fixture.bc -o fixture.S
    ```
    
    `make fixture.S`:
    
    ```
    	.text
    	.file	"fixture.c"
    	.globl	test                            ; -- Begin function test
    	.type	test,@function
    test:                                   ; @test
    ; %bb.0:
    	add	x0, x1, x0
    	ret
    .Lfunc_end0:
    	.size	test, .Lfunc_end0-test
                                            ; -- End function
    	.ident	"clang version 20.0.0git (https://github.com/andresag01/llvm-project.git 399ab535adc597355a29d41e072a9eed6d9b1f8b)"
    	.section	".note.GNU-stack","",@progbits
    ```
    
    It seems to be working (ignore git origin since I cloned andresag01's fork)

--- clang/lib/Basic/Targets.cpp
+++ clang/lib/Basic/Targets.cpp
@@ -457,7 +457,7 @@ std::unique_ptr<TargetInfo> AllocateTarget(const llvm::Triple &Triple,
     }
 
   case llvm::Triple::riscw:
-    return new RISCWTargetInfo(Triple, Opts);
+    return std::make_unique<RISCWTargetInfo>(Triple, Opts);
 
   case llvm::Triple::sparc:
     switch (os) {
